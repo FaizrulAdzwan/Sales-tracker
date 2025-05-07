c3bf01b2ae6433d57e9af892909730e5ee0692c9
FaizrulAdzwan <faizruladzwan@gmail.com>
2025-05-07 19:13:24 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,20 @@
-
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"; // Import Toaster
 import SWRegister from '../components/SWRegister';
-
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
 });
-
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
 });
-
 export const metadata: Metadata = {
   title: 'Sales Insights', // Updated title
   description: 'Track daily sales performance.', // Updated description
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -27,7 +22,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}><SWRegister />
-
         {children}
         <Toaster /> {/* Add Toaster component here */}
       </body>
