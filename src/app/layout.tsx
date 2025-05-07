4f8d59d72166b8e520d9d4e8455ae077d92699c9
FaizrulAdzwan <faizruladzwan@gmail.com>
2025-05-07 19:04:59 +0800

    Move service worker registration to SWRegister component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-'use client';
+
 import type { Metadata } from 'next';
-import { useEffect } from 'react';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"; // Import Toaster
+import SWRegister from '../components/SWRegister';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -24,19 +24,10 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-          .then((reg) => console.log('✅ Service worker registered:', reg))
-          .catch((err) => console.error('❌ Service worker failed:', err));
-      });
-    }
-  }, []);
-
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}><SWRegister />
+
         {children}
         <Toaster /> {/* Add Toaster component here */}
       </body>
