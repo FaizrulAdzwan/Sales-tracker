01fb079e4b714e12bf3bf04693eef189ddb0d248
FaizrulAdzwan <faizruladzwan@gmail.com>
2025-05-07 18:23:46 +0800

    Create head.tsx

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>CDS Sales Tracker</title>
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" href="/icon-192.png" />
+      <meta name="theme-color" content="#0d3b66" />
+    </>
+  )
+}
